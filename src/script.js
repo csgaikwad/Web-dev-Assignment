478dedb6a09d9bbc484194d46012a5bc5ca99240
csgaikwad <csgaikwad321@gmail.com>
2024-07-14 23:48:57 +0530

    second part completed

--- src/script.js
+++ src/script.js
@@ -1,47 +1,24 @@
 $(document).ready(function () {
-  // Function to handle placeholder behavior
-  function handlePlaceholder(input, placeholder) {
-    // Function to check input value and update placeholder class
-    function checkInputValue() {
-      if ($(input).val() !== "") {
-        $(placeholder).addClass("active");
-      } else {
-        $(placeholder).removeClass("active");
-      }
-    }
-
-    // Check input value on page load
-    checkInputValue();
-
-    // Add event listener for input focus
-    $(input).on("focus", function () {
-      $(placeholder).addClass("active");
-    });
-
-    // Add event listener for input blur (when focus is lost)
-    $(input).on("blur", function () {
-      checkInputValue();
-    });
-  }
-
-  // Call the function for each input
-  handlePlaceholder("#email", "#emailPlaceholder");
-  handlePlaceholder("#firstName", "#firstNamePlaceholder");
-  handlePlaceholder("#lastName", "#lastNamePlaceholder");
-
-  // Scroll behavior for navigation
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 5) {
-      $("nav").css({
-        "box-shadow": "0 5px 5px rgba(0, 0, 0, 0.2)",
-        "background-color": "white",
-      });
-    } else {
+  // Function to handle navigation scroll behavior
+  function handleNavScroll() {
+    if ($(window).scrollTop() < 5) {
       $("nav").css({
         "box-shadow": "none",
         "background-color": "transparent",
       });
+    } else {
+      $("nav").css({
+        "box-shadow": "0 5px 5px rgba(0, 0, 0, 0.2)",
+        "background-color": "white",
+      });
     }
+  }
+  // Check scroll position on page load
+  handleNavScroll();
+
+  // Scroll behavior for navigation
+  $(window).scroll(function () {
+    handleNavScroll();
   });
 
   // Modal padding adjustment
@@ -53,6 +30,31 @@ $(document).ready(function () {
     $("nav, body").css("padding-right", "0");
   });
 
+  // Handle placeholder behavior
+  function handlePlaceholder(input, placeholder) {
+    function checkInputValue() {
+      if ($(input).val() !== "") {
+        $(placeholder).addClass("active");
+      } else {
+        $(placeholder).removeClass("active");
+      }
+    }
+
+    checkInputValue();
+
+    $(input).on("focus", function () {
+      $(placeholder).addClass("active");
+    });
+
+    $(input).on("blur", function () {
+      checkInputValue();
+    });
+  }
+
+  handlePlaceholder("#email", "#emailPlaceholder");
+  handlePlaceholder("#firstName", "#firstNamePlaceholder");
+  handlePlaceholder("#lastName", "#lastNamePlaceholder");
+
   // Form submission handling with Axios
   $(".contactUsForm").on("submit", function (event) {
     event.preventDefault();
@@ -63,9 +65,7 @@ $(document).ready(function () {
       lastName: $("#lastName").val(),
       termsCheckbox: $("#termsCheckbox").is(":checked"),
     };
-    // console.log(formData);
 
-    // Make POST request with Axios
     axios
       .post("https://getform.io/f/aejyjzob", formData)
       .then((response) => {
@@ -76,7 +76,7 @@ $(document).ready(function () {
           title: "Success!",
           text: "Form submitted successfully.",
           icon: "success",
-          confirmButtonText: "OK"
+          confirmButtonText: "OK",
         });
       })
       .catch((error) => {
@@ -85,8 +85,29 @@ $(document).ready(function () {
           title: "Error!",
           text: "Something went wrong. Please try again later.",
           icon: "error",
-          confirmButtonText: "OK"
+          confirmButtonText: "OK",
         });
       });
   });
+
+  // Function to handle dot click
+  const carousel = $(".carousel");
+  const dots = $(".dot");
+  let currentIndex = 0;
+
+  dots.on("click", function () {
+    dots.removeClass("active");
+    $(this).addClass("active");
+    currentIndex = $(this).index();
+    const imgWidth = carousel.find("img").first().width() + 14;
+    const scrollAmount = currentIndex * imgWidth * 3;
+    carousel.scrollLeft(scrollAmount);
+  });
+
+  function autoScroll() {
+    currentIndex = (currentIndex + 1) % dots.length;
+    dots.eq(currentIndex).click();
+  }
+
+  setInterval(autoScroll, 4000);
 });
